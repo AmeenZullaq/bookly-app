e5c61ea8fa6f5221580aae6010183cf9523e1a71
ameen <ameenemails@gmail.com>
2023-08-24 14:01:25 +0300

    add rating to book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_bookly/core/utils/styles.dart';
+import 'package:my_bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:my_bookly/features/home/presentation/views/widgets/custom_book_image.dart';
-
 import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -13,7 +13,7 @@ class BookDetailsViewBody extends StatelessWidget {
       children: [
         const CustomBookDetailsAppBar(),
         SizedBox(
-          height: MediaQuery.of(context).size.height * .35,
+          height: MediaQuery.of(context).size.height * .3,
           child: const CustomBookImage(),
         ),
         const SizedBox(
@@ -35,6 +35,12 @@ class BookDetailsViewBody extends StatelessWidget {
               fontWeight: FontWeight.w400,
             ),
           ),
+        ),
+        const SizedBox(
+          height: 18,
+        ),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
         ),
       ],
     );
--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import '../../../../../core/utils/styles.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
+  const BookRating({
+    super.key,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+  });
+
+  final MainAxisAlignment mainAxisAlignment;
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
-          Icons.star,
+          FontAwesomeIcons.solidStar,
+          size: 14,
           color: Color(0xffFFDD4F),
         ),
         const SizedBox(
@@ -22,10 +30,13 @@ class BookRating extends StatelessWidget {
         const SizedBox(
           width: 5,
         ),
-        Text(
-          '(2390)',
-          style: Styles.textStyle14.copyWith(
-            color: const Color(0xff707070),
+        Opacity(
+          opacity: .5,
+          child: Text(
+            '(2390)',
+            style: Styles.textStyle14.copyWith(
+              fontWeight: FontWeight.w600
+            ),
           ),
         ),
       ],
