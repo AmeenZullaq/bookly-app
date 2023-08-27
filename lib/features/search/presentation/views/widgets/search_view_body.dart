933d8990cbba7a04ddf8b04edfb92c9c66eeeec4
ameen <ameenemails@gmail.com>
2023-08-27 12:39:04 +0300

    search view done

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/styles.dart';
+import '../../../../home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -11,10 +13,44 @@ class SearchViewBody extends StatelessWidget {
         horizontal: 30,
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomSearchTextField(),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Search Result',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Expanded(
+            child: ResultSearchListView(),
+          ),
         ],
       ),
+    );
+  }
+}
+
+class ResultSearchListView extends StatelessWidget {
+  const ResultSearchListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: EdgeInsets.zero,
+      itemBuilder: (context, index) {
+        return const BookListViewItem();
+      },
+      separatorBuilder: (context, index) {
+        return const SizedBox(
+          height: 20,
+        );
+      },
+      itemCount: 10,
     );
   }
 }
