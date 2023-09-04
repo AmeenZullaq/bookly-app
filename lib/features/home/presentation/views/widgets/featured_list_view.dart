875ce3cd692febc4a34c11177181fd1d913104c4
ameen <ameenemails@gmail.com>
2023-09-04 10:42:59 +0300

    display featured books part 1

--- lib/features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_bookly/core/widgets/custom_error_widget.dart';
+import 'package:my_bookly/core/widgets/custom_loading_indecator.dart';
+import 'package:my_bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:my_bookly/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'custom_book_image.dart';
 
 class FeaturedListView extends StatelessWidget {
@@ -6,20 +11,31 @@ class FeaturedListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.separated(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const CustomBookImage();
-        },
-        separatorBuilder: (context, index) {
-          return const SizedBox(
-            width: 10,
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          List<BookModel> books = state.books;
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .3,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return CustomBookImage();
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(
+                  width: 10,
+                );
+              },
+              itemCount: 10,
+            ),
           );
-        },
-        itemCount: 10,
-      ),
+        } else if (state is FeaturedBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndecator();
+        }
+      },
     );
   }
 }
